b2ef4c133f6fc25bf3c3705ff03c9ac76724bc26
sankyk2020 <69984129+sanky2020@users.noreply.github.com>
2022-03-17 16:47:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    items: 1,
+    loop:true,
+    center:true,
+    autoplay:true,
+    margin:10,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+  
+  // You can use responsive item here based on requirement;  
+    // responsive:{
+    //     0:{
+    //         items:1
+    //     },
+    //     600:{
+    //         items:3
+    //     },
+    //     1000:{
+    //         items:5
+    //     }
+    // }
+  })
